87f734c8cc16e9de413591fe330e3d21c54e2022
Piratt14 <satdvlad@gmail.com>
2019-12-11 00:52:55 +0300

    added 2 pages and basic routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './app.css';
+import { Route, Link, Switch } from 'react-router-dom';
+import { HomePage, CartPage } from "../pages";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <Switch>
+                <Route path="/" component={HomePage} exact/>
+                <Route path="/cart" component={CartPage}/>
+            </Switch>
+        </div>
+    );
 };
 
 export default App;
