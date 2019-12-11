9ddfe13c9f9baf4bfa869e59f774be2135b62bb3
Piratt14 <satdvlad@gmail.com>
2019-12-11 14:37:04 +0300

    show header in main component and wrap all content in container(bootstrap)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './app.css';
 import { Route, Link, Switch } from 'react-router-dom';
+import ShopHeader from "../shop-header";
 import { HomePage, CartPage } from "../pages";
 
 const App = () => {
     return (
-        <div>
+        <main role="main" className="container">
+            <ShopHeader numItems={5} total={310}/>
             <Switch>
                 <Route path="/" component={HomePage} exact/>
                 <Route path="/cart" component={CartPage}/>
             </Switch>
-        </div>
+        </main>
     );
 };
 
