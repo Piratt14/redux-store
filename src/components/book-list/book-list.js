61630b81e2e8ce6c3a345d59caf55c0b5eefd6d7
Piratt14 <satdvlad@gmail.com>
2019-12-11 23:10:34 +0300

    restructuring update according to state

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -48,7 +48,7 @@ class BookListContainer extends Component {
 }
 
 //  эта функция описывает какие данных компонент хочет получить из REDUX STORE
-const mapStateToProps = ({ books, loading, error }) => {
+const mapStateToProps = ({ bookList: { books, loading, error }}) => {
     return {
         books,
         loading,
