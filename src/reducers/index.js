cc1ee4c04bca74b72fe875a5acb42ec848d5ae06
Piratt14 <satdvlad@gmail.com>
2019-12-11 22:11:55 +0300

    wrote a separate universal function for updating an item and implemented the removal of one unit of goods from the list and the removal of all units of goods from the list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -30,7 +30,7 @@ const updateCartItems = (cartItems, item, idx) => {
     ];
 };
 
-const updateCartItem = (book, item = {} ) => {
+const updateCartItem = (book, item = {}, quantity) => {
 
     //  если item === undefined(то есть не найден в массиве корзины)
     //  то ему присваивается пустой объект, а затем объект деструктурируеся
@@ -45,9 +45,23 @@ const updateCartItem = (book, item = {} ) => {
     return {
         id,
         title,
-        count: count + 1,
-        total: total + book.price,
+        count: count + quantity,
+        total: total + quantity*book.price,
     }
+};
+
+const updateOrder = (state, bookId, quantity) => {
+    const { books, cartItems } = state;
+
+    const book = books.find(({id}) => id === bookId);
+    const itemIndex = cartItems.findIndex(({id}) => id === bookId);
+    const item = cartItems[itemIndex];
+
+    const newItem = updateCartItem(book, item, quantity);
+    return {
+        ...state,
+        cartItems: updateCartItems(cartItems, newItem, itemIndex),
+    };
 };
 
 const reducer = (state = initialState, action) => {
@@ -77,16 +91,14 @@ const reducer = (state = initialState, action) => {
             };
 
         case 'BOOK_ADDED_TO_CART':
-            const bookId = action.payload;
-            const book = state.books.find((book) => book.id === bookId);
-            const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
-            const item = state.cartItems[itemIndex];
+            return updateOrder(state, action.payload, 1);
 
-            const newItem = updateCartItem(book, item);
-            return {
-                ...state,
-                cartItems: updateCartItems(state.cartItems, newItem, itemIndex),
-            };
+        case 'BOOK_REMOVED_FROM_CART':
+            return updateOrder(state, action.payload, -1);
+
+        case 'ALL_BOOKS_REMOVED_FROM_CART':
+            const item = state.cartItems.find(({id}) => id === action.payload);
+            return updateOrder(state, action.payload, -item.count);
 
         default:
             return state;
