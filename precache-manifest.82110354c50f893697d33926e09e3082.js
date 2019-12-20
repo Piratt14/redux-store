81db8a238ce1c31d073f432c8a2c2a6f30e572d5
Piratt14 <satdvlad@gmail.com>
2019-12-20 22:32:22 +0300

    Updates

---
+++ precache-manifest.82110354c50f893697d33926e09e3082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89bff8cdcd74c5132fadff0114158e13",
+    "url": "/redux-store/index.html"
+  },
+  {
+    "revision": "8e46365605ffc8aad754",
+    "url": "/redux-store/static/css/main.f3a2abb6.chunk.css"
+  },
+  {
+    "revision": "daf8c824a439c2a6fd0f",
+    "url": "/redux-store/static/js/2.8d29723c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/redux-store/static/js/2.8d29723c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e46365605ffc8aad754",
+    "url": "/redux-store/static/js/main.a03d4a6a.chunk.js"
+  },
+  {
+    "revision": "8c1da62b4573880bb46e",
+    "url": "/redux-store/static/js/runtime-main.3fc14cab.js"
+  }
+]);
